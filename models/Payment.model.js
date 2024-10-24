2b8c7c66304490ea946c1451003abee7ddda0352
Angela Ruiz <angela.ruiz.rodriguez.8@gmail.com>
2024-10-24 13:55:51 +0200

    merge angela

--- models/Payment.model.js
+++ models/Payment.model.js
@@ -1,23 +1,24 @@
-
 const mongoose = require("mongoose");
 
 const paymentSchema = new mongoose.Schema({
-  price: Number, 
-  paymentIntentId: String, 
-  clientSecret: String, 
+  price: Number,
+  paymentIntentId: String,
+  clientSecret: String,
   status: {
     type: String,
     enum: ["incomplete", "succeeded"],
     default: "incomplete",
   },
-  product: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Product"
-  },
-//   buyer: {
-//     type: mongoose.Schema.Types.ObjectId,
-//     ref: "User"
-//   },
+  product: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+    },
+  ],
+  //   buyer: {
+  //     type: mongoose.Schema.Types.ObjectId,
+  //     ref: "User"
+  //   },
 });
 
 const Payment = mongoose.model("Payment", paymentSchema);
