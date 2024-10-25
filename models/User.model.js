36fef8d1a6441ec1fd1b1bfba4f11e1d9541f156
r.ruizfer <r.ruizfer97@gmail.com>
2024-10-25 09:10:22 +0200

    ajustes Users

--- models/User.model.js
+++ models/User.model.js
@@ -6,7 +6,7 @@ const userSchema = new Schema(
   {
     googleId: {
       type: String,
-      required: true,
+
       unique: true,
     },
     email: {
@@ -39,7 +39,7 @@ const userSchema = new Schema(
     image: {
       type: String,
       default:
-        "https://static.wikia.nocookie.net/joke-battles/images/d/df/Gigachad.png/revision/latest?cb=20230812064835",
+        "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
     },
     isAdmin: {
       type: Boolean,
